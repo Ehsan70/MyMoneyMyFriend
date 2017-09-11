0edd6bd4b389f03e8d64daee58e1c955a6d235be
Ehsan70 <a.ehsan70@gmail.com>
2017-09-11 00:10:05 -0700

    Created a Greeting Service

---
+++ src/MyMoneyMyFriend/Greeter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMoneyMyFriend
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration )
+        {
+            //ASP.NET does not know any services that implements IConfiguration. The configuration was created in the startup and is stored in Configuration property. So another service has to be register.  
+            _greeting = configuration["Greeting"];
+        }
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
--- src/MyMoneyMyFriend/Startup.cs
+++ src/MyMoneyMyFriend/Startup.cs
@@ -32,10 +32,23 @@ namespace MyMoneyMyFriend
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Igreeting has to be registered so that ASP.NEt knows about it. 
+            // Note that There are some built-in services that ASP.NEt will provide by default. In particular, the services variables will have 16 Interfaces already configured. 
+            // So once a service that implements IGreeter is registered. ASP.NEt will be able to pass that along to any component that is controlled by ASP.NET that needs it.  
+            /*
+             services.AddSingleton() : Add a single instans of the service. Everyone sees the same instance. 
+             services.AddTransient() : Add a service with a transient life time. Anytime that service is needed by a method or component, that service is reinstantiated.   
+             services.AddScoped() : Adds a service that will be scoped to HTTP request. All the components inside of a single request will see a same instance, but accross two different HTTP requests will be two different instances.  
+            */
+            // Since an instance of that object is available we can just pass that object. ASP.NET is smart enough to see the type of this and so matches that type to this instance.  
+            services.AddSingleton(Configuration);
+            //Bellow tells ASP.NET that when ever you see IGreeter, you need to instanciate and pass in the Greeter class.  
+            services.AddSingleton<IGreeter, Greeter>();
+            // So Greeter needs and configuration. ASP.NET realizes that Greeter needs a configuration and therefore passes Configuration instance to Greeter. 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. HTTP processing pipeline is configured here. 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -47,7 +60,7 @@ namespace MyMoneyMyFriend
             app.Run(async (context) =>
             {
                 var message = Configuration["Greeting"];
-                await context.Response.WriteAsync(message);
+                await context.Response.WriteAsync(greeter.GetGreeting());
             });
         }
     }
