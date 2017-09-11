9c4bf31ce96fe2d13092af98b1ab96472a9fe3bb
Ehsan70 <a.ehsan70@gmail.com>
2017-09-10 23:35:01 -0700

    Added a Configuration Source

--- src/MyMoneyMyFriend/Startup.cs
+++ src/MyMoneyMyFriend/Startup.cs
@@ -7,18 +7,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace MyMoneyMyFriend
 {
+    /* Asp.net will look for `StartUp` class by convention. In side of this class, application is configured or the configuration sources are configured.   
+     */
     public class Startup
     {
+        // This will be executed before ConfigureServices and Configure. 
+        public Startup(IHostingEnvironment env)
+        {
+            // Tell ASP.NET about the configuration file so that it can read it in. 
+            // Telling the configuration builder about my base path; i.e. where to look for files by default.  
+            var builder = new ConfigurationBuilder().SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json").AddEnvironmentVariables();
+            // Build a configuration for me that will return an object that implement IConfiguration.  
+            Configuration = builder.Build();    
+            
+        }
+
+        public  IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. HTTP processing pipeline is configured here. 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
@@ -30,7 +46,8 @@ namespace MyMoneyMyFriend
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var message = Configuration["Greeting"];
+                await context.Response.WriteAsync(message);
             });
         }
     }
